560a19054ff217e183898d8f05fa26a416f8a96f
Carla Holcomb <carla.programmer@gmail.com>
2020-01-25 18:40:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+  "Hello, World!"
+end
+
+get '/' do
+  "Hello, World!"
+end
+
+get '/' do
+  "Hello, World!"
+end
+
 end
